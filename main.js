33f4dfd4d366f59f39eee44bec4b12c68656dcb2
Matt <matt@coolermaster>
2020-08-24 21:59:20 +1200

    define constants for some server settings

--- main.js
+++ main.js
@@ -6,22 +6,26 @@ const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
 
+const SERVER_URL = "http://localhost:1337/api"
+const APP_NAME = "TotalCoats"
+
 const api = new ParseServer({
-  databaseURI: 'mongodb://localhost:27017/test', // Connection string for your MongoDB database
-  cloud: __dirname + '/functions/main.js', // Absolute path to your Cloud Code
+  databaseURI: 'mongodb://localhost:27017/test',
+  // cloud: __dirname + '/functions/main.js', // Absolute path to your Cloud Code
   appId: process.env.PARSE_APP_ID,
   masterKey: process.env.PARSE_MASTER_KEY, // Keep this key secret!
-  serverURL: 'http://localhost:1337/api', // Don't forget to change to https if needed
+  serverURL: SERVER_URL, // Don't forget to change to https if needed
   restApiKey: process.env.PARSE_REST_API_KEY,
+  allowClientClassCreation: false,
 });
 
 const dashboard = new ParseDashboard({
   "apps": [
     {
-      "serverURL": "http://localhost:1337/api",
+      "serverURL": SERVER_URL,
       "appId": process.env.PARSE_APP_ID,
       "masterKey": process.env.PARSE_MASTER_KEY,
-      "appName": "TotalCoats"
+      "appName": APP_NAME,
     }
   ]
 })
@@ -30,10 +34,16 @@ app.use('/api', api)
 
 app.use('/dashboard', dashboard)
 
+app.use('/public', express.static(__dirname + '../totalcoats-mithril-frontend/public', ))
+
 app.get('/', (req, res) => {
   res.json('Hello, developer.')
 })
 
 app.listen(1337, function() {
   console.log('Server listening on port 1337')
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.message)
 })
