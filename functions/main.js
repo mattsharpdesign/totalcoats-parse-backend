76e2a292af1ce2b2fe64487c0d2fe1b4279e27eb
Matt Sharp <mattsharpdesign@gmail.com>
2021-01-03 00:06:31 +1300

    add fetch jobs by status to cloud functions

--- functions/main.js
+++ functions/main.js
@@ -1,15 +1,15 @@
 /*
-** Test cloud function
-*/
-Parse.Cloud.beforeFind('Customer', async req => {
-  console.log('finding customers...')
-})
+ * Test cloud function
+ */
+// Parse.Cloud.beforeFind('Customer', async req => {
+//   console.log('finding customers...')
+// })
 
 
 
 /*
-** Add jobNumber (auto increment) to new jobs
-*/
+ * Add jobNumber (auto increment) to new jobs
+ */
 Parse.Cloud.beforeSave('Job', async req => {
   if (!req.object.get('jobNumber')) {
     const Job = Parse.Object.extend('Job')
@@ -23,5 +23,31 @@ Parse.Cloud.beforeSave('Job', async req => {
     } else {
       req.object.set('jobNumber', 1)
     }
+  }
+})
+
+/*
+ * Fetch jobs by status if requested
+ */
+Parse.Cloud.beforeFind('Job', async req => {
+  if (req.query._where.status) {
+    const { status } = req.query._where
+    let originalQuery = req.query
+    let statusQuery = new Parse.Query('Job')
+    switch (status) {
+      case 'unfinished':
+        let dneQuery = new Parse.Query('Job').doesNotExist('quantityDelivered')
+        let zeroQuery = new Parse.Query('Job').equalTo('quantityDelivered', 0)
+        let emptyQuery = new Parse.Query('Job').equalTo('quantityDelivered', '')
+        statusQuery = Parse.Query.or(dneQuery, zeroQuery, emptyQuery)
+        break;
+      case 'finished':
+        statusQuery.greaterThanOrEqualTo('quantityDelivered', 1)
+        break;
+      default:
+        console.log(`Unknown status "${status}" provided to job query`)
+    }
+    delete originalQuery._where.status
+    return Parse.Query.and(statusQuery, originalQuery)
   }
 })
